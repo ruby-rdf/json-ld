ff996e8f55c1e81a7a898d9d5391e2b4b57a4532
Gregg Kellogg <gregg@kellogg-assoc.com>
2011-05-09 16:21:30 -0700

    Note that format accepts :json as well as :ld. However, :json conflicts with rdf/json.

--- lib/json/ld/format.rb
+++ lib/json/ld/format.rb
@@ -1,26 +1,31 @@
-module RDF::N3
+module JSON::LD
   ##
   # RDFa format specification.
   #
   # @example Obtaining an Notation3 format class
-  #     RDF::Format.for(:jld)            #=> JSON::LD::Format
-  #     RDF::Format.for("etc/foaf.jld")
-  #     RDF::Format.for(:file_name      => "etc/foaf.jld")
-  #     RDF::Format.for(:file_extension => "jld")
+  #     RDF::Format.for(:json)            #=> JSON::LD::Format
+  #     RDF::Format.for(:ld)              #=> JSON::LD::Format
+  #     RDF::Format.for("etc/foaf.json")
+  #     RDF::Format.for("etc/foaf.ld")
+  #     RDF::Format.for(:file_name      => "etc/foaf.json")
+  #     RDF::Format.for(:file_name      => "etc/foaf.ld")
+  #     RDF::Format.for(:file_extension => "json")
+  #     RDF::Format.for(:file_extension => "ld")
   #     RDF::Format.for(:content_type   => "application/json")
   #
   # @example Obtaining serialization format MIME types
   #     RDF::Format.content_types      #=> {"application/json" => [JSON::LD::Format]}
   #
   # @example Obtaining serialization format file extension mappings
-  #     RDF::Format.file_extensions    #=> {:ttl => "application/json"}
+  #     RDF::Format.file_extensions    #=> {:json => "application/json"}
   #
   # @see http://www.w3.org/TR/rdf-testcases/#ntriples
   class Format < RDF::Format
-    content_type     'application/json',    :extension => :jld
+    content_type     'application/json',    :extension => :json
+    content_type     'application/json',    :extension => :ld
     content_encoding 'utf-8'
 
-    reader { RDF::N3::Reader }
-    writer { RDF::N3::Writer }
+    reader { JSON::LD::Reader }
+    writer { JSON::LD::Writer }
   end
 end
