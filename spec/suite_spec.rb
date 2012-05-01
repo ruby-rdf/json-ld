49242c80071ba39ee796c052881291ad30c040dd
Gregg Kellogg <gregg@kellogg-assoc.com>
2012-04-30 19:19:19 -0700

    Update toRDF suite spec to use API.toRDF instead of reader. Implemented custom to_quad() to output UTF-8.

--- spec/suite_spec.rb
+++ spec/suite_spec.rb
@@ -44,14 +44,14 @@ describe JSON::LD do
                   expected = JSON.load(t.expect)
                   result.should produce(expected, t.debug)
                 when Fixtures::JSONLDTest::ToRDFTest
-                  reader = RDF::Reader.open(t.inputDocument,
-                    :base => t.inputDocument,
-                    :debug => t.debug,
-                    :format => :jsonld)
-                  reader.should be_a JSON::LD::Reader
+                  quads = []
+                  JSON::LD::API.toRDF(t.input, nil, nil,
+                                      :base => t.inputDocument,
+                                      :debug => t.debug) do |statement|
+                    quads << to_quad(statement)
+                  end
 
-                  graph = RDF::Repository.new << reader
-                  graph.dump(:nquads).should produce(t.expect.read, t.debug)
+                  quads.sort.join("").should produce(t.expect.read, t.debug)
                 else
                   pending("unkown test type #{t.inspect}")
                 end
@@ -67,5 +67,38 @@ describe JSON::LD do
         end
       end
     end
+  end
+  
+  # Don't use NQuads writer so that we don't escape Unicode
+  def to_quad(thing)
+    case thing
+    when RDF::URI
+      "<#{escaped(thing.to_s)}>"
+    when RDF::Node
+      escaped(thing.to_s)
+    when RDF::Literal::Double
+      quoted("%1.15e" % thing.value) + "^^<#{RDF::XSD.double}>"
+    when RDF::Literal
+      quoted(escaped(thing.value)) +
+      (thing.datatype? ? "^^<#{thing.datatype}>" : "") +
+      (thing.language? ? "@#{thing.language}" : "")
+    when RDF::Statement
+      thing.to_quad.map {|r| to_quad(r)}.compact.join(" ") + " .\n"
+    end
+  end
+
+  ##
+  # @param  [String] string
+  # @return [String]
+  def quoted(string)
+    "\"#{string}\""
+  end
+
+  ##
+  # @param  [String] string
+  # @return [String]
+  def escaped(string)
+    string.gsub('\\', '\\\\').gsub("\t", '\\t').
+      gsub("\n", '\\n').gsub("\r", '\\r').gsub('"', '\\"')
   end
 end
