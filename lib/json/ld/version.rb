076dd7f6bc8207396477088e829e6bf75e2a0864
Gregg Kellogg <gregg@kellogg-assoc.com>
2013-05-19 17:42:17 -0700

    Fix version reporting.

--- lib/json/ld/version.rb
+++ lib/json/ld/version.rb
@@ -1,6 +1,6 @@
 module JSON::LD::VERSION
   VERSION_FILE = File.join(File.expand_path(File.dirname(__FILE__)), "..", "..", "..", "VERSION")
-  MAJOR, MINOR, TINY, EXTRA = File.read(VERSION_FILE).chop.split(".")
+  MAJOR, MINOR, TINY, EXTRA = File.read(VERSION_FILE).chomp.split(".")
 
   STRING = [MAJOR, MINOR, TINY, EXTRA].compact.join('.')
 
