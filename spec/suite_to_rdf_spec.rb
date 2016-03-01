f43198e96308249f1e12fc16fc994e142601bea7
Gregg Kellogg <gregg@kellogg-assoc.com>
2016-03-01 14:45:35 -0700

    Use BNode namer when generating nodes for toRdf, rather than re-using the input symbol. This was formerly done in flattening.
    (cherry picked from commit aa9131e8f7e7a38300cbf51868326f83b800441e)

--- spec/suite_to_rdf_spec.rb
+++ spec/suite_to_rdf_spec.rb
@@ -11,7 +11,6 @@ describe JSON::LD do
         specify "#{t.property('input')}: #{t.name}#{' (negative test)' unless t.positiveTest?}" do
           skip "Native value fidelity" if %w(toRdf-0035-in.jsonld).include?(t.property('input'))
           pending "Generalized RDF" if %w(toRdf-0118-in.jsonld).include?(t.property('input'))
-          pending "Blank nodes with reverse properties" if %w(toRdf-0119-in.jsonld).include?(t.property('input'))
           t.run self
         end
       end
