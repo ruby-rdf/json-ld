647aa8a9d36affdd269f462ffea5ed67dff94648
Gregg Kellogg <gregg@kellogg-assoc.com>
2016-03-23 14:34:42 -0700

    Update cli_commands to add help and description.

--- spec/format_spec.rb
+++ spec/format_spec.rb
@@ -75,37 +75,37 @@ describe JSON::LD::Format do
 
     describe "#expand" do
       it "expands RDF" do
-        expect {RDF::CLI.exec_command("expand", [ttl], format: :ttl)}.to write.to(:output)
+        expect {RDF::CLI.exec(["expand", ttl], format: :ttl)}.to write.to(:output)
       end
       it "expands JSON" do
-        expect {RDF::CLI.exec_command("expand", [json], format: :jsonld)}.to write.to(:output)
+        expect {RDF::CLI.exec(["expand", json], format: :jsonld)}.to write.to(:output)
       end
     end
 
     describe "#compact" do
       it "compacts RDF" do
-        expect {RDF::CLI.exec_command("compact", [ttl], context: context, format: :ttl)}.to write.to(:output)
+        expect {RDF::CLI.exec(["compact", ttl], context: context, format: :ttl)}.to write.to(:output)
       end
       it "compacts JSON" do
-        expect {RDF::CLI.exec_command("compact", [json], context: context, format: :jsonld)}.to write.to(:output)
+        expect {RDF::CLI.exec(["compact", json], context: context, format: :jsonld)}.to write.to(:output)
       end
     end
 
     describe "#flatten" do
       it "flattens RDF" do
-        expect {RDF::CLI.exec_command("flatten", [ttl], context: context, format: :ttl)}.to write.to(:output)
+        expect {RDF::CLI.exec(["flatten", ttl], context: context, format: :ttl)}.to write.to(:output)
       end
       it "flattens JSON" do
-        expect {RDF::CLI.exec_command("flatten", [json], context: context, format: :jsonld)}.to write.to(:output)
+        expect {RDF::CLI.exec(["flatten", json], context: context, format: :jsonld)}.to write.to(:output)
       end
     end
 
     describe "#frame" do
       it "frames RDF" do
-        expect {RDF::CLI.exec_command("frame", [ttl], frame: context, format: :ttl)}.to write.to(:output)
+        expect {RDF::CLI.exec(["frame", ttl], frame: context, format: :ttl)}.to write.to(:output)
       end
       it "frames JSON" do
-        expect {RDF::CLI.exec_command("frame", [json], frame: context, format: :jsonld)}.to write.to(:output)
+        expect {RDF::CLI.exec(["frame", json], frame: context, format: :jsonld)}.to write.to(:output)
       end
     end
   end
