0f597301ef6880988508f12088ca5f3e9a91b1d9
Gregg Kellogg <gregg@greggkellogg.net>
2017-05-27 12:31:57 -0700

    When testing, use local path for RDF file loads if it exists.

--- spec/suite_helper.rb
+++ spec/suite_helper.rb
@@ -1,6 +1,70 @@
 require 'json/ld'
 require 'support/extensions'
 
+
+# For now, override RDF::Utils::File.open_file to look for the file locally before attempting to retrieve it
+module RDF::Util
+  module File
+    REMOTE_PATH = "http://json-ld.org/test-suite/"
+    LOCAL_PATH = ::File.expand_path("../json-ld.org/test-suite", __FILE__) + '/'
+
+    class << self
+      alias_method :original_open_file, :open_file
+    end
+
+    ##
+    # Override to use Patron for http and https, Kernel.open otherwise.
+    #
+    # @param [String] filename_or_url to open
+    # @param  [Hash{Symbol => Object}] options
+    # @option options [Array, String] :headers
+    #   HTTP Request headers.
+    # @return [IO] File stream
+    # @yield [IO] File stream
+    def self.open_file(filename_or_url, options = {}, &block)
+      case 
+      when filename_or_url.to_s =~ /^file:/
+        path = filename_or_url[5..-1]
+        Kernel.open(path.to_s, options, &block)
+      when (filename_or_url.to_s =~ %r{^#{REMOTE_PATH}} && Dir.exist?(LOCAL_PATH))
+        #puts "attempt to open #{filename_or_url} locally"
+        localpath = filename_or_url.to_s.sub(REMOTE_PATH, LOCAL_PATH)
+        response = begin
+          ::File.open(localpath)
+        rescue Errno::ENOENT => e
+          raise IOError, e.message
+        end
+        document_options = {
+          base_uri:     RDF::URI(filename_or_url),
+          charset:      Encoding::UTF_8,
+          code:         200,
+          headers:      {}
+        }
+        #puts "use #{filename_or_url} locally"
+        document_options[:headers][:content_type] = case filename_or_url.to_s
+        when /\.ttl$/    then 'text/turtle'
+        when /\.nt$/     then 'application/n-triples'
+        when /\.jsonld$/ then 'application/ld+json'
+        else                  'unknown'
+        end
+
+        document_options[:headers][:content_type] = response.content_type if response.respond_to?(:content_type)
+        # For overriding content type from test data
+        document_options[:headers][:content_type] = options[:contentType] if options[:contentType]
+
+        remote_document = RDF::Util::File::RemoteDocument.new(response.read, document_options)
+        if block_given?
+          yield remote_document
+        else
+          remote_document
+        end
+      else
+        original_open_file(filename_or_url, options, &block)
+      end
+    end
+  end
+end
+
 module Fixtures
   module SuiteTest
     SUITE = RDF::URI("http://json-ld.org/test-suite/")
