7ac50659de3dc446438eae27c26eac66d0e048ce
Gregg Kellogg <gregg@greggkellogg.net>
2019-06-14 10:29:44 -0700

    Mark toRdf 0130 pending until RDF.rb addresses odd IRI joining issue.

--- spec/suite_to_rdf_spec.rb
+++ spec/suite_to_rdf_spec.rb
@@ -10,6 +10,7 @@ describe JSON::LD do
         specify "#{t.property('@id')}: #{t.name}#{' (negative test)' unless t.positiveTest?}" do
           skip "Native value fidelity" if %w(toRdf/0035-in.jsonld).include?(t.property('input'))
           pending "Generalized RDF" if %w(toRdf/0118-in.jsonld).include?(t.property('input'))
+          pending "Non-heirarchical IRI joining" if %w(toRdf/0130-in.jsonld).include?(t.property('input'))
           t.run self
         end
       end
