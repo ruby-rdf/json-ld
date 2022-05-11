869a47377a64e40619708460067fc6152f4c0ada
Gregg Kellogg <gregg@greggkellogg.net>
2022-05-10 22:16:29 -0700

    Skip some tests on Windows.

--- spec/format_spec.rb
+++ spec/format_spec.rb
@@ -70,7 +70,7 @@ describe JSON::LD::Format do
     end
   end
 
-  describe ".cli_commands" do
+  describe ".cli_commands", skip: Gem.win_platform? do
     require 'rdf/cli'
     let(:ttl) {File.expand_path("../test-files/test-1-rdf.ttl", __FILE__)}
     let(:json) {File.expand_path("../test-files/test-1-input.json", __FILE__)}
